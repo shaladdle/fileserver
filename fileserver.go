483384222e4197ebde89ef034a1c476175aa6589
Adam Wright <adamwright@cmu.edu>
2014-03-17 04:18:06 -0400

    TLS working

--- fileserver.go
+++ fileserver.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "flag"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
 )
 
 var (
-    certFile = flag.String("cert", "", "")
-    keyFile = flag.String("key", "", "")
-    root = flag.String("root", "", "")
+	certFile = flag.String("cert", "", "")
+	keyFile  = flag.String("key", "", "")
+	root     = flag.String("root", "", "")
+	port     = flag.Int("port", -1, "")
 )
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    handler := http.FileServer(http.Dir(*root))
-    log.Fatal(http.ListenAndServeTLS(":8080", *certFile, *keyFile, handler))
+	absRoot, err := filepath.Abs(*root)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	absKey, err := filepath.Abs(*keyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	absCert, err := filepath.Abs(*certFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("args: root = %s, cert = %s, key = %s", absRoot, absCert, absKey)
+
+	handler := http.FileServer(http.Dir(absRoot))
+	hostport := fmt.Sprintf(":%d", *port)
+	log.Fatal(http.ListenAndServeTLS(hostport, absCert, absKey, handler))
 }
